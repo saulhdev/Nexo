370f00c99682925fc2493e4a2753fbd6ed824b70
Saul Henriquez <saul_henriquez@hotmail.com>
2026-08-16 12:12:10 -0600

    Show current page in title

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { watchEffect } from 'vue'
+import { RouterView, useRoute } from 'vue-router'
+import { useI18n } from '@/i18n'
+
+const route = useRoute()
+const { t } = useI18n()
+
+watchEffect(() => {
+  const titleKey = route.meta?.titleKey as string | undefined
+  if (titleKey) {
+    document.title = `${t(titleKey)} | Nexo App`
+  } else {
+    document.title = 'Nexo App'
+  }
+})
 </script>
 
 <template>
   <RouterView />
 </template>
+
